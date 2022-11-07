bcbbd82f0d57e169843193fc3a655209f2554fa4
shaquille19 <shakil.m.19@gmail.com>
2022-11-07 21:52:30 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -11,6 +12,21 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Plastic Surgeon Service Website is Running.");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f1cm5cm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
 
 app.listen(port, () =>
   console.log(`Plastic Surgeon Service Website is Running on Port ${port}`)
